b14afd611f5d639655c6b47af85582629b132130
Vamshi Krishna <vamshi.qaeng@gmail.com>
2023-09-01 11:39:55 +0530

    Uppercase example

--- src/StringPrograms/removeDuplicate.java
+++ src/StringPrograms/removeDuplicate.java
@@ -3,8 +3,24 @@ package StringPrograms;
 public class removeDuplicate {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+     String name ="Vamshi Krishna";
+     System.out.println(removeDuplicateChar(name));
+    
+     
 	}
+	public static String removeDuplicateChar(String str)
+	{
+	    String newStr="";
+	    for(int i=0; i<str.length();i++)
+	    {
+	    	char ch = str.charAt(i);
+	    	if(newStr.indexOf(ch)==-1)
+	    	{
+	    		newStr+=ch;
+	    	}
+	    }
+	    return newStr ;
+	}
+	
 
 }
