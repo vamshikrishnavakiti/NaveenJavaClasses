1c8ccd2719705e9f206d2a28a5ac68f09ae690ec
Vamshi Krishna <vamshi.qaeng@gmail.com>
2023-09-01 12:58:44 +0530

    Adding more prgrams

--- src/Arrays/arraysConcepts.java
+++ src/Arrays/arraysConcepts.java
@@ -1,7 +1,22 @@
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class arraysConcepts {
 	
-	int epm_ID[] = new int[100];
+	public static void main(String[] args) {
+		ArrayList<String> li = new ArrayList<String>();
+		li.add("Vamshi");
+		li.add("Naveen");
+		li.add("Sai");
+		
+		Iterator it = li.iterator();
+		while(it.hasNext());
+		System.out.println(it.next());
+		
+		
+	}
 
 }
